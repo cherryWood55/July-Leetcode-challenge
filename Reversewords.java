f4e101801665ebef06c34eb3a2014583d07a5a1a
Diya Nag Chaudhury <44759773+cherryWood55@users.noreply.github.com>
2020-07-16 08:01:08 +0530

    Added Problem link and Time complexity

--- Reversewords.java
+++ Reversewords.java
@@ -1,4 +1,7 @@
+//Problem Link : https://leetcode.com/explore/featured/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3391/
+
 //Reverse words in a string
+
 import java.util.*;
 class Main {
     public static String reverseWords(String s) {
@@ -10,17 +13,15 @@ class Main {
         for (int i=0; i<len; i++)
         {
             char ch = s.charAt(i);
-            if (ch == ' ')
+            if (ch == ' ')//if the selected character is a whitespace
             {
-                String str = s.substring(pos, i);
-                
-                new_str = str.trim() + " " + new_str;
-                
-                pos = i + 1;
+                String str = s.substring(pos, i);//extract the word before the space
+                new_str = str.trim() + " " + new_str;//add it to the new string
+                pos = i + 1;//increment pos to point to the beginning of the next word
             }
         }
-        new_str = new_str.trim();
-        new_str = new_str.replaceAll("\\s+", " ");
+        new_str = new_str.trim();//remove all trailing zeroes, if any
+        new_str = new_str.replaceAll("\\s+", " ");//replace the multiple spaces with a single space
         return new_str;
     }
     public static void main(String[] args)
@@ -29,3 +30,5 @@ class Main {
         System.out.println(reverseWords(s));
     }
 }
+
+//Time complexity : O(N) where N is the length of the string
